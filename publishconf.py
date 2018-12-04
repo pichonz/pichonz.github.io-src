26ecccde74013432fce51a1a3eb0211eadf3a262
david <hernandrojas@gmail.com>
2018-12-03 22:03:05 -0800

    updates

--- publishconf.py
+++ publishconf.py
@@ -18,6 +18,10 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = False
 
+# prevent tag pages from populating
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
+
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = 'hedaro'
